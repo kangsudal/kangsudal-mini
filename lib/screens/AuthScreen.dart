a0c6e575b535c0247a646feba80f5b3251a0933b
kangsudal_notebook <kangsudal@gmail.com>
2023-04-14 12:48:01 +0900

    인증페이지에서 뒤로가기 눌렀을때 내자산 탭으로 못가게 막기

---
+++ lib/screens/AuthScreen.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+class AuthScreen extends StatelessWidget {
+  const AuthScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+      ),
+      body: Stack(
+        children: [
+          Positioned(
+            left: 10,
+            right: 10,
+            bottom: 10,
+            //VerticalDivider는 SizedBox의 height을 필요로한다.
+            child: SizedBox(
+              height: 50,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    '인증/OTP',
+                    style: TextStyle(
+                      color: Colors.grey,
+                    ),
+                  ),
+                  VerticalDivider(
+                    width: 20,
+                    thickness: 0.3,
+                    indent: 19,
+                    endIndent: 15,
+                    color: Colors.white,
+                  ),
+                  Text(
+                    'KB증권 계좌개설',
+                    style: TextStyle(
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'K-mini',
+                  style: TextStyle(
+                    fontSize: 50,
+                    color: Theme.of(context).primaryColor,
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 80,
+                    vertical: 5,
+                  ),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Theme.of(context).primaryColor,
+                    ),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Text(
+                    '간편인증 로그인',
+                    style: TextStyle(
+                      color: Theme.of(context).primaryColor,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:kangsudal_mini/screens/AuthScreen.dart';
 import 'package:kangsudal_mini/screens/MoreScreen.dart';
 import 'package:kangsudal_mini/state/page_index.dart';
 
@@ -161,15 +162,20 @@ class HomeScreen extends ConsumerWidget {
             ),
           ],
           onTap: (int selectedIdx) {
-            ref
-                .read(bottomNavigatorSelectedIndex.notifier)
-                .update((state) => selectedIdx);
+            //내자산 탭을 눌렀을때 로그인 상태가 아니면 탭을 바꾸지않고 인증페이지로 이동한다.
             if (selectedIdx == 1) {
               if (!ref.watch(isLogin)) {
-                Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => AuthScreen(),
-                ));
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => AuthScreen(),
+                  ),
+                );
               }
+            } else {
+              //로그인 상태면 탭을 바꿔준다.
+              ref
+                  .read(bottomNavigatorSelectedIndex.notifier)
+                  .update((state) => selectedIdx);
             }
           },
           currentIndex: selectedIndex,
@@ -337,89 +343,4 @@ class HomeAppBar extends StatelessWidget with PreferredSizeWidget {
 
   @override
   Size get preferredSize => Size.fromHeight(kToolbarHeight * 2);
-}
-
-class AuthScreen extends StatelessWidget {
-  const AuthScreen({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(),
-      body: Stack(
-        children: [
-          Positioned(
-            left: 10,
-            right: 10,
-            bottom: 10,
-            //VerticalDivider는 SizedBox의 height을 필요로한다.
-            child: SizedBox(
-              height: 50,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    '인증/OTP',
-                    style: TextStyle(
-                      color: Colors.grey,
-                    ),
-                  ),
-                  VerticalDivider(
-                    width: 20,
-                    thickness: 0.3,
-                    indent: 19,
-                    endIndent: 15,
-                    color: Colors.white,
-                  ),
-                  Text(
-                    'KB증권 계좌개설',
-                    style: TextStyle(
-                      color: Colors.grey,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Align(
-            alignment: Alignment.center,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'K-mini',
-                  style: TextStyle(
-                    fontSize: 50,
-                    color: Theme.of(context).primaryColor,
-                  ),
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                Container(
-                  padding: EdgeInsets.symmetric(
-                    horizontal: 80,
-                    vertical: 5,
-                  ),
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Theme.of(context).primaryColor,
-                    ),
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: Text(
-                    '간편인증 로그인',
-                    style: TextStyle(
-                      color: Theme.of(context).primaryColor,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
 }
