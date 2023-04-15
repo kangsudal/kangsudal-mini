1c4ae28c442c47621711998d9564c8be13f1d2dd
kangsudal_notebook <kangsudal@gmail.com>
2023-04-15 11:56:48 +0900

    메인화면 Rive 에셋 추가

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kangsudal_mini/screens/HomeScreen.dart';
+import 'package:rive/rive.dart';
 
 void main() {
   runApp(
@@ -44,19 +45,27 @@ class StartPage extends StatelessWidget {
               height: 100,
             ),
             DefaultTabController(
-              length: 3,
+              length: 4,
               child: Column(
                 children: [
                   Container(
                     width: 200,
-                    height: 200,
+                    height: 300,
                     child: TabBarView(
                       children: [
-                        Placeholder(),
-                        Placeholder(),
-                        Placeholder(),
+                        riveWithText('assets/calendar.riv',
+                            '원하는 기간동안\n적립식으로 꾸준히\n정기구매해요!'),
+                        riveWithText(
+                            'assets/coin.riv', '비싼 주식도\n천원부터 원하는 만큼\n구매해요!'),
+                        riveWithText(
+                            'assets/data.riv', '유명한 투자대가의\n포트폴리오를 보고\n투자해요!'),
+                        riveWithText(
+                            'assets/files.riv', '오늘 봐야하는\n투자정보만\n콕! 콕! 집어드려요!'),
                       ],
                     ),
+                  ),
+                  SizedBox(
+                    height: 10,
                   ),
                   TabPageSelector(
                     borderStyle: BorderStyle.none,
@@ -97,6 +106,38 @@ class StartPage extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+
+  Column riveWithText(String riv, String title) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(14),
+          child: Container(
+            width: 200,
+            height: 200,
+            child: RiveAnimation.asset(
+              riv,
+              fit: BoxFit.contain,
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 15,
+        ),
+        Container(
+          child: Text(
+            textAlign: TextAlign.center,
+            title,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
